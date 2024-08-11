825e8d171f76cc3025806c106e58606c9c202a83
Matt Hinchliffe <matt@maketea.co.uk>
2024-08-11 16:36:19 +0100

    Upgrade ESLint from v8 to v9

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+]
